ebf66a91ab356976b366e6936e1a14789d5f3e0d
Cyber Knight <cyberknight755@gmail.com>
2026-09-03 21:27:42 +0000

    input/misc: Import fingerprint driver from `MMI-U1TNS34.82-12-7-23`
    - This driver was imported from [1].
    
    [1]: https://github.com/MotorolaMobilityLLC/motorola-kernel-modules/tree/MMI-U1TNS34.82-12-7-23/drivers/input/misc/chipone_fps
    
    Change-Id: If062009db62cc487e69c726d46b9841ec6706f01
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

---
+++ drivers/input/misc/chipone_fps/Android.mk
@@ -0,0 +1,26 @@
+DLKM_DIR := motorola/kernel/modules
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(CHIPONE_GPIO_CTRL_VDD),true)
+    KERNEL_CFLAGS  += CONFIG_CHIPONE_GPIO_CTRL_VDD=y
+endif
+
+ifeq ($(CHIPONE_PMIC_CTRL_VDD),true)
+    KERNEL_CFLAGS  += CONFIG_CHIPONE_PMIC_CTRL_VDD=y
+endif
+
+ifeq ($(CHIPONE_MTK_KERNEL5XX_WAKE_TYPE), true)
+    KERNEL_CFLAGS  += CONFIG_CHIPONE_MTK_KERNEL5XX_WAKE_TYPE=y
+endif
+
+ifeq ($(MOTO_FPS_PRECISE_POWERON), true)
+    KERNEL_CFLAGS += CONFIG_MOTO_FPS_PRECISE_POWERON=y
+endif
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fpsensor_mtk_spi.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
